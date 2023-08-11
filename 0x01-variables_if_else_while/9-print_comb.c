5a80cfe993df420799f20fb41007ff6a4e966764
Noran <noranmustafa97@yahoo.com>
2023-08-11 15:39:56 -0700

    k

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,11 +15,12 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		if (i != 8)
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n')
 	return (0);
 }

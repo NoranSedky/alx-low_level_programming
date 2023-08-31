e38655f2e0d17db647e9d345d465334cf146dc6a
root <root@DESKTOP-Q29J8BH>
2023-08-31 19:57:42 +0300

    re

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The input number.
+ * Return: Natural square root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
+
+/**
+ * sqrt - helper
+ * @n: The input number.
+ * @val: sqare root.
+ * Return: int
+ */
+
+int sqrt(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (sqrt(n, val + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Checks if a number is a prime number.
+ * @n: The input number.
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_helper(n, 2));
+	}
+}
+
+/**
+ * is_prime_helper - Checks if a number is a prime number.
+ * @n: The input number.
+ * @divisor: int
+ * Return: int
+ */
+
+int is_prime_helper(int n, int divisor)
+{
+	if (divisor >= n)
+	{
+		return (1);
+	}
+	else if (n % divisor == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_helper(n, divisor + 1));
+	}
+}
